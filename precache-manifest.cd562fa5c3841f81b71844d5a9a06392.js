76687a4a4b71f2cbf9df34a3a03edc4be7b91ecb
Aniket Thakor <you@example.com>
2022-04-26 19:56:20 +0530

    Updates

---
+++ precache-manifest.cd562fa5c3841f81b71844d5a9a06392.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f4fe0b2949d1bc4b5abf60f54b6fee",
+    "url": "/e-commerce-site/index.html"
+  },
+  {
+    "revision": "378178775f8e6fd683c5",
+    "url": "/e-commerce-site/static/css/2.ca8e03c8.chunk.css"
+  },
+  {
+    "revision": "636ae673c9b6075990fc",
+    "url": "/e-commerce-site/static/css/main.92b95ad5.chunk.css"
+  },
+  {
+    "revision": "378178775f8e6fd683c5",
+    "url": "/e-commerce-site/static/js/2.20d0d62b.chunk.js"
+  },
+  {
+    "revision": "636ae673c9b6075990fc",
+    "url": "/e-commerce-site/static/js/main.32150305.chunk.js"
+  },
+  {
+    "revision": "96e03a7676a3475c66a8",
+    "url": "/e-commerce-site/static/js/runtime~main.4f1da623.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/e-commerce-site/static/media/logo.9f7e506d.svg"
+  }
+]);
